2de445ac4b0af97c96ac65d9b028f8d5019f4bc8
Vannah-22 <vannjackson16@gmail.com>
2021-03-21 23:43:30 -0600

    catching up on React training

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router, 
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header"> 
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code> src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https:reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <div>
+                    <Router>
+                        <div>
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <Link to="/"> Home </Link>
+                                    </li>
+
+                                    <li>
+                                        <Link to="/about"> About </Link>
+                                    </li> 
+                                </ul>
+                            </nav>
+                        </div>
+                    </Router>
+                </div>
             </header>
         </div>
     )
