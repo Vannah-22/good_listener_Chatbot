0ae963a3072393fff163012f57f25bcf64d58cea
Vannah-22 <vannjackson16@gmail.com>
2021-03-21 22:48:32 -0600

    catching up/starting over on App.js

---
+++ src/components/Head/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+    render() {
+        return (     
+            <header className = "header">
+                <h1 className="header__title"> EMA </h1>
+            </header>
+        )
+    }
+}
+export default Header;
