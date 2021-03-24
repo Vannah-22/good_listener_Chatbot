0f136f6ef0fe9d06b59e8a0cb4ae0b3333b7350a
Vannah-22 <vannjackson16@gmail.com>
2021-03-23 22:19:24 -0600

    EMA's welcome homepage with text input and submit button

--- src/components/Body/Welcome.js
+++ src/components/Body/Welcome.js
@@ -4,12 +4,34 @@ import './Welcome.css';
 class Welcome extends React.Component {
     render() {
         return (     
-            <h3 className = "welcome">
-                <h5 className="welcome__info">
+            <div className = "welcome">
+                <h5 className="welcome__greeting">
                     Hi! My name is Ema. I'm an Emotion Management Assistant 
                     and I am here to provide you with support!
                 </h5>
-            </h3>
+
+                <h5 className="welcome__greeting"> What's your name? </h5>
+                <p> 
+                    <input type="text" id="usersName" name="the_name"
+                    placeholder="What can I call you?" required autofocus />
+                    <input type="submit" id="usersNameSubmit" value="Continue"></input>
+                </p>
+
+
+                
+                {/* <button>
+                    Happy
+                </button>
+
+                <button>
+                    Happy
+                </button>
+
+                <button>
+                    Happy
+                </button> */}
+
+            </div>
         )
     }
 }
