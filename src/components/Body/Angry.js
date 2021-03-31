412c0e113769e0399cdb9dada9dbd2a518037b26
Vannah-22 <vannjackson16@gmail.com>
2021-03-30 23:42:14 -0600

    creating an onClick feature for Angry/grief, back to square 1

--- src/components/Body/Angry.js
+++ src/components/Body/Angry.js
@@ -5,51 +5,62 @@ import {
     Link
 } from "react-router-dom";
 
-function madReturns () {
-    var madReturns= new Array ("I'm sorry you're grieving, but you do not have to face it alone",
-    "You are safe to grieve at your own pace", 
-    "Don't forget to reach out to family and friends in your time of need")
-    
-    var random=madReturns[Math.floor(Math.random() * madReturns.length)];
-    document.getElementById("griefReturns").innerText=random;
+class Angry extends React.Component {
+    render() {
+        return (     
+            <div>
+                <h5 className="std-msg">
+                    I'm sorry you feel this way. Can you explain why?
+                </h5>
 
-    return 
+                <Router>
+                    <div className= "mood-select">
+                        <Link to="/howareyoufeelingtoday/angry/grief"
+                            type="button"
+                            className="std-mood-buttons">
+                            I am grieving
+                        </Link>  
+
+                        <Link to="/howareyoufeelingtoday/angry/frustration"
+                            type="button"
+                            className="std-mood-buttons"> 
+                            I feel alone
+                        </Link>
+
+                        <Link to="/howareyoufeelingtoday/angry/fearful"
+                            type="button"
+                            className="std-mood-buttons"> 
+                            I am scared
+                        </Link>
+                        </div>
+                </Router>
+            </div>
+        )
     }
+}
+export default Angry;
 
-    console.log(madReturns);
-//         return (     
-//             <div>
-//                 <h5 className="std-msg">
-//                     I'm sorry you feel this way. Can you explain why?
-//                 </h5>
-
-//                 <Router>
-//                     <div className= "mood-select">
-//                         <Link to="/howareyoufeelingtoday/angry/grief"
-//                             type="button"
-//                             className="std-mood-buttons"
-//                             onClick={madReturns.random}> 
-//                             I am grieving
-//                         </Link>  
-
-//                         <Link to="/howareyoufeelingtoday/angry/frustration"
-//                             type="button"
-//                             className="std-mood-buttons"> 
-//                             I feel alone
-//                         </Link>
-
-//                         <Link to="/howareyoufeelingtoday/angry/fearful"
-//                             type="button"
-//                             className="std-mood-buttons"> 
-//                             I am scared
-//                         </Link>
-//                         </div>
-//                 </Router>
-//             </div>
-//         )
-// export default Angry;
-export default madReturns;
-
-
+//ATEMPTS //
+// function madReturns () {
+//     var madReturns= new Array ("I'm sorry you're grieving, but you do not have to face it alone",
+//     "You are safe to grieve at your own pace", 
+//     "Don't forget to reach out to family and friends in your time of need")
     
+//     var random=madReturns[Math.floor(Math.random() * madReturns.length)];
+//     document.getElementById("griefReturns").innerHTML=random;
+//     }
+//     onClick={madReturns.random}> 
+
+// function madReturns () {
+//         var madReturns= new Array ("I'm sorry you're grieving, but you do not have to face it alone",
+//         "You are safe to grieve at your own pace", 
+//         "Don't forget to reach out to family and friends in your time of need")
+        
+//         var random=madReturns[Math.floor(Math.random() * madReturns.length)];
+//         document.getElementById("griefReturns").innerText=random;
+    
+//         return 
+//         }
+    
+//         console.log(madReturns);
  
