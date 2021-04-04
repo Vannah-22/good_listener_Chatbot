ed03c8c7276749f0d43a49e6bbe95f42311e46a3
Vannah-22 <vannjackson16@gmail.com>
2021-04-04 06:52:17 -0600

    resources added to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,13 +5,27 @@ class Footer extends React.Component {
     render() {
         return (     
             <footer className = "footer">
-                <h5 className="footer__info">
+                <p className="footer__info">
                     Copyright 
                     <br /> 
                     Vanessa Jackson 2021
                     <br />
                     Art by Lauren Jackson 2021
-                </h5>
+                </p>
+
+                <ul className="resources">
+                    <li> 
+                        <a href= "https://suicidepreventionlifeline.org" target="_blank"> 
+                            National Suicide Prevention Lifeline Website 
+                        </a>
+                    </li>
+                    <li> 
+                        <p> 800-273-8255  </p>
+                    </li>
+                    
+                </ul>
+                
+                
             </footer>
         )
     }
