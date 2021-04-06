f334139804d91ebd80da11c508e02e303fcfee07
Vannah-22 <vannjackson16@gmail.com>
2021-04-06 02:13:37 -0600

    correct counter for AngerGrief button

--- src/components/Body/Angry.js
+++ src/components/Body/Angry.js
@@ -3,7 +3,6 @@ import './MoodSelect.css';
 import {
     Link
 } from "react-router-dom";
-import AngryGrief from './angryGrief';
 import Typing from 'react-typing-animation';
 export default Angry;
 
@@ -23,38 +22,57 @@ function Angry() {
                         type="button"
                         className="std-mood-buttons"
                         onClick = {
-                            {AngryGrief}
-                        }
-                        >
-                    </Link> 
-
-                    <Link to="/howareyoufeelingtoday/angry/grief"
-                        type="button"
-                        className="std-mood-buttons"
-                        id="madGrief"
-                        onClick = {
                             function returnCount () {
-                                    setCount (count+1);
-                                }
+                                setCount (count+1);
+                            }
                         }>
-                        {count}
+                        I am grieving {count}
+                    </Link>  
+
+                    <Link to="/howareyoufeelingtoday/angry/frustration"
+                        type="button"
+                        className="std-mood-buttons"> 
+                        I feel frustrated
                     </Link>
 
-                    <Link to="/howareyoufeelingtoday/angry/grief"
+                    <Link to="/howareyoufeelingtoday/angry/fearful"
                         type="button"
-                        className="std-mood-buttons"
-                        id="madGrief"
-                        onClick = {
-                            function returnCount () {
-                                    setCount (count+1);
-                                }
-                        }>
-                        {count}
+                        className="std-mood-buttons"> 
+                        I am scared
                     </Link>
-
-                    
                     </div>
-                    
         </div>
-    ) 
-}     
+    )
+
+
+
+
+
+}
+        
+
+
+//ATEMPTS //
+// function madReturns () {
+//     var madReturns= new Array ("I'm sorry you're grieving, but you do not have to face it alone",
+//     "You are safe to grieve at your own pace", 
+//     "Don't forget to reach out to family and friends in your time of need")
+    
+//     var random=madReturns[Math.floor(Math.random() * madReturns.length)];
+//     document.getElementById("griefReturns").innerHTML=random;
+//     }
+//     onClick={madReturns.random}> 
+
+// function madReturns () {
+//         var madReturns= new Array ("I'm sorry you're grieving, but you do not have to face it alone",
+//         "You are safe to grieve at your own pace", 
+//         "Don't forget to reach out to family and friends in your time of need")
+        
+//         var random=madReturns[Math.floor(Math.random() * madReturns.length)];
+//         document.getElementById("griefReturns").innerText=random;
+    
+//         return 
+//         }
+    
+//         console.log(madReturns);
+ 
--- src/components/Body/angryGrief.js
+++ src/components/Body/angryGrief.js
@@ -5,34 +5,48 @@ import {
 } from "react-router-dom";
 
 import Typing from 'react-typing-animation';
-export default AngryGrief;
+export default Angry;
 
-var griefReturns = ["text1", "text2", "text3"];
-function AngryGrief() {
+function Angry() {
     const [count, setCount] = useState(0);
 
+function Grief() {
+    const [statement, setStatement] = useState(0);
+}
     return (     
         <div>
             <Typing> 
                 <p className="std-msg">
-                    here is some text from grief
+                    I'm sorry you feel this way. Can you explain why?
                 </p>
             </Typing>
+            
 
-            <div className= "mood-select">
+                <div className= "mood-select">
                     <Link to="/howareyoufeelingtoday/angry/grief"
                         type="button"
                         className="std-mood-buttons"
-                        id="madGrief"
                         onClick = {
                             function returnCount () {
-                                document.getElementById("madGrief").props
-                                    setCount (count+1);
-                                }
+                                setCount (count+1);
+                            }
                         }>
-                        {count}
-                    </Link> 
-            </div>
+                        I am grieving {count}
+                    </Link>  
+
+                    <Link to="/howareyoufeelingtoday/angry/frustration"
+                        type="button"
+                        className="std-mood-buttons"> 
+                        I feel frustrated
+                    </Link>
+
+                    <Link to="/howareyoufeelingtoday/angry/fearful"
+                        type="button"
+                        className="std-mood-buttons"> 
+                        I am scared
+                    </Link>
+                    </div>
+                    
         </div>
-    )
-}
+    ) 
+}     
